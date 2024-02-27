0bb776ddff0347195aad43acbc98a246a253cbfb
Emmanuel <96310496+eulasi@users.noreply.github.com>
2024-02-27 00:40:06 -0500

    file refactor

--- app/src/main/java/com/example/tfl/MainActivity.kt
+++ app/src/main/java/com/example/tfl/MainActivity.kt
@@ -3,40 +3,8 @@ package com.example.tfl
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.tfl.data.networking.TflItem
-import com.example.tfl.utilities.generateRandomColor
-import com.example.tfl.viewmodel.TubeStatusViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import androidx.compose.runtime.livedata.observeAsState
 import com.example.tfl.ui.composables.TubeStatusScreen
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
